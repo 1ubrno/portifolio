34f1dbdc6ea0903143ee5beeebc66325191da3d0
Bruno <brnonobre@gmail.com>
2025-07-04 17:07:57 -0400

    update
    primeira parte do codigo

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,29 @@
+// animação do parágrafo
 const text = 'Hi! Im Bruno Nobre, a developer from Brazil';
-  const target = document.getElementById("typewriter-target");
-  let index = 0;
+const target = document.getElementById("typewriter-target");
+let index = 0;
 
-  function type() {
-    if (index < text.length) {
-      target.innerHTML += text.charAt(index);
-      index++;
-      setTimeout(type, 50); // velocidade da digitação (ms)
-    }
+function type() {
+  if (index < text.length) {
+    target.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(type, 50);
   }
+}
 
-  window.addEventListener("DOMContentLoaded", type);
+window.addEventListener("DOMContentLoaded", type);
 
+// animação do botão + scroll suave
+const button = document.getElementById("seeMyWork-btn");
 
-//adiciona o movimento suave
-document.getElementById("seeMyWork-btn").addEventListener('click',()=> {
-    document.getElementById('projects').scrollIntoView({behavior:'smooth'})
-})
+button.addEventListener("click", () => {
+  // animação do botão
+  button.classList.add("pulse-animation");
+
+  setTimeout(() => {
+    button.classList.remove("pulse-animation");
+  }, 400);
+
+  // scroll suave
+  document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
