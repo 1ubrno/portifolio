648af6ce0b8e2610615c8617ea72feae78d0a141
Bruno <brnonobre@gmail.com>
2025-07-08 15:51:07 -0400

    segunda secao
    estrutura da segunda secao e correcao na barra nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,30 @@
 // animação do parágrafo
-const text = 'Hi! Im Bruno Nobre, a developer from Brazil';
-const target = document.getElementById("typewriter-target");
-let index = 0;
+const text = 'Hi! Im Bruno Nobre, a developer from Brazil'
+const target = document.getElementById("typewriter-target")
+let index = 0
 
 function type() {
   if (index < text.length) {
-    target.innerHTML += text.charAt(index);
+    target.innerHTML += text.charAt(index)
     index++;
-    setTimeout(type, 50);
+    setTimeout(type, 50)
   }
 }
 
-window.addEventListener("DOMContentLoaded", type);
+
+window.addEventListener("DOMContentLoaded", type)
 
 // animação do botão + scroll suave
-const button = document.getElementById("seeMyWork-btn");
+const button = document.getElementById("seeMyWork-btn")
 
 button.addEventListener("click", () => {
   // animação do botão
-  button.classList.add("pulse-animation");
+  button.classList.add("pulse-animation")
 
   setTimeout(() => {
-    button.classList.remove("pulse-animation");
+    button.classList.remove("pulse-animation")
   }, 400);
 
   // scroll suave
-  document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
-});
+  document.getElementById('projects').scrollIntoView({ behavior: 'smooth' })
+})
